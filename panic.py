7a25cab6751f66343d503ee70a94d7bd472d4748
defeller <denver26.87@gmail.com>
2020-02-28 23:18:44 +0300

    add vowels7

---
+++ panic.py
@@ -0,0 +1,14 @@
+phrase = "Don't panic!"
+plist = list(phrase)
+#plist1 = plist.copy[1: 8]
+print(phrase)
+print(plist)
+
+new_phrase = ''.join(plist[1:3])
+new_phrase = new_phrase + ''.join([plist[5], plist[4], plist[7], plist[6]])
+
+
+#new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
+
